d22aa0cef7949c3a7537fd5d0678a5f79c75aca2
cl6016 <cl6016@nyu.edu>
2022-03-17 09:41:10 -0400

    Gateway

--- GatewayService/src/main/java/com/java/APIGatewayApplication.java
+++ GatewayService/src/main/java/com/java/APIGatewayApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
+//https://www.jianshu.com/p/b23ff932746b
 
 @SpringBootApplication
 public class APIGatewayApplication {
